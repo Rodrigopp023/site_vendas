8c69b87c072f2af51ef32fd281f8d530ccee1497
Rodrigo <rodrigo.paz.2310@gmail.com>
2025-07-08 15:12:17 -0300

    Entidade Pedido

---
+++ src/main/java/com/portfolio/site/entities/Pedido.java
@@ -0,0 +1,94 @@
+package com.portfolio.site.entities;
+
+import com.portfolio.site.enums.StatusPagamento;
+import com.portfolio.site.entities.Usuario;
+import jakarta.persistence.*;
+
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_pedido")
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant dataCompra;
+    private StatusPagamento status;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
+
+    @OneToOne
+    @JoinColumn(name = "endereco_entrega_id")
+    private EnderecoEntrega enderecoEntrega;
+
+    public Pedido(Long id, Instant dataCompra, StatusPagamento status,
+                  Usuario usuario, EnderecoEntrega enderecoEntrega) {
+        this.id = id;
+        this.dataCompra = dataCompra;
+        this.status = status;
+        this.usuario = usuario;
+        this.enderecoEntrega = enderecoEntrega;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getDataCompra() {
+        return dataCompra;
+    }
+
+    public void setDataCompra(Instant dataCompra) {
+        this.dataCompra = dataCompra;
+    }
+
+    public StatusPagamento getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusPagamento status) {
+        this.status = status;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public EnderecoEntrega getEnderecoEntrega() {
+        return enderecoEntrega;
+    }
+
+    public void setEnderecoEntrega(EnderecoEntrega enderecoEntrega) {
+        this.enderecoEntrega = enderecoEntrega;
+    }
+
+    public Double valorFinal(ItemPedido item) {
+        return item.subTotal();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Pedido pedido = (Pedido) o;
+        return Objects.equals(id, pedido.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
---
+++ src/main/java/com/portfolio/site/entities/Role.java
@@ -0,0 +1,52 @@
+package com.portfolio.site.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String perfil;
+
+    public Role() {
+    }
+
+    public Role(Long id, String perfil) {
+        this.id = id;
+        this.perfil = perfil;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPerfil() {
+        return perfil;
+    }
+
+    public void setPerfil(String perfil) {
+        this.perfil = perfil;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return Objects.equals(perfil, role.perfil);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(perfil);
+    }
+}
+
