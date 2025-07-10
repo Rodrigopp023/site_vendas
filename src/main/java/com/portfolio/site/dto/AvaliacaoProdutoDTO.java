63f312f474d61d0261de22e13ffa157088952a51
Rodrigo <rodrigo.paz.2310@gmail.com>
2025-07-10 16:18:50 -0300

    DTO 'Classe AvaliacaoProdutoDTO'

---
+++ src/main/java/com/portfolio/site/dto/AvaliacaoProdutoDTO.java
@@ -0,0 +1,51 @@
+package com.portfolio.site.dto;
+
+import com.portfolio.site.entities.AvaliacaoProduto;
+
+import java.time.Instant;
+
+public class AvaliacaoProdutoDTO {
+    private Long id;
+    private Double nota;
+    private String comentario;
+    private Instant dataAvaliacao;
+    private UsuarioDTO usuario;
+
+
+    public AvaliacaoProdutoDTO(Long id, Double nota, String comentario,
+                               Instant dataAvaliacao, UsuarioDTO usuario) {
+        this.id = id;
+        this.nota = nota;
+        this.comentario = comentario;
+        this.dataAvaliacao = dataAvaliacao;
+        this.usuario = usuario;
+    }
+
+    public AvaliacaoProdutoDTO(AvaliacaoProduto entidade) {
+        id = entidade.getId();
+        nota = entidade.getNota();
+        comentario = entidade.getComentario();
+        dataAvaliacao = entidade.getDataAvaliacao();
+        usuario = new UsuarioDTO(entidade.getUsuario());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Double getNota() {
+        return nota;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public Instant getDataAvaliacao() {
+        return dataAvaliacao;
+    }
+
+    public UsuarioDTO getUsuario() {
+        return usuario;
+    }
+}
