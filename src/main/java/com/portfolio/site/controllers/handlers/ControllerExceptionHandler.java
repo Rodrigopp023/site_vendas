8e3a3b5b28812b617a55099234f80f8e40ebc4bf
Rodrigo <rodrigo.paz.2310@gmail.com>
2025-07-15 14:09:00 -0300

    Implementado a classe 'ControllerExceptionHandler'

---
+++ src/main/java/com/portfolio/site/controllers/handlers/ControllerExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.portfolio.site.controllers.handlers;
+
+import com.portfolio.site.dto.CustomErrorDTO;
+import com.portfolio.site.services.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<CustomErrorDTO> resourceNotFound(
+            ResourceNotFoundException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        CustomErrorDTO errorDTO = new CustomErrorDTO(
+                Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(errorDTO);
+    }
+}
