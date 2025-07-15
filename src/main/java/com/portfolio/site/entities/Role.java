df108d2b6dd1aca01539ec9aaa693e839e03eebf
Rodrigo <rodrigo.paz.2310@gmail.com>
2025-07-15 20:47:36 -0300

    Atualização da classe Role, mudou o nome de 'perfil' para 'authority'

--- src/main/java/com/portfolio/site/entities/Role.java
+++ src/main/java/com/portfolio/site/entities/Role.java
@@ -2,23 +2,25 @@ package com.portfolio.site.entities;
 
 import jakarta.persistence.*;
 
+import org.springframework.security.core.GrantedAuthority;
+
 import java.util.Objects;
 
 @Entity
 @Table(name = "tb_role")
-public class Role {
+public class Role implements GrantedAuthority{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String perfil;
+    private String authority;
 
     public Role() {
     }
 
-    public Role(Long id, String perfil) {
+    public Role(Long id, String authority) {
         this.id = id;
-        this.perfil = perfil;
+        this.authority = authority;
     }
 
     public Long getId() {
@@ -29,24 +31,25 @@ public class Role {
         this.id = id;
     }
 
-    public String getPerfil() {
-        return perfil;
+    @Override
+    public String getAuthority() {
+        return authority;
     }
 
-    public void setPerfil(String perfil) {
-        this.perfil = perfil;
+    public void setAuthority(String authority) {
+        this.authority = authority;
     }
 
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         Role role = (Role) o;
-        return Objects.equals(perfil, role.perfil);
+        return Objects.equals(authority, role.authority);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hashCode(perfil);
+        return Objects.hashCode(authority);
     }
 }
 
