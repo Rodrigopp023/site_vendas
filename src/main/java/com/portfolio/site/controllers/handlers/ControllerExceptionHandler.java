da3621450a62c7f86d035abe8bb685db102b6415
Rodrigo <rodrigo.paz.2310@gmail.com>
2025-07-15 19:56:08 -0300

    Atualização da classe ControllerExceptionHandler 'Implementando DataBaseException'

--- src/main/java/com/portfolio/site/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/portfolio/site/controllers/handlers/ControllerExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.portfolio.site.controllers.handlers;
 
 import com.portfolio.site.dto.CustomErrorDTO;
+import com.portfolio.site.services.exceptions.DatabaseException;
 import com.portfolio.site.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -19,6 +20,16 @@ public class ControllerExceptionHandler {
         HttpStatus status = HttpStatus.NOT_FOUND;
         CustomErrorDTO errorDTO = new CustomErrorDTO(
                 Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(errorDTO);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<CustomErrorDTO> databaseException(
+            DatabaseException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        CustomErrorDTO errorDTO = new CustomErrorDTO(
+                Instant.now(), status.value(), e.getMessage(),
+                request.getRequestURI());
         return ResponseEntity.status(status).body(errorDTO);
     }
 }
