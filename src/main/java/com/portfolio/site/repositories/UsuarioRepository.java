4c92ab59026c02687c3ea7a7e9b2662de1609da9
Rodrigo <rodrigo.paz.2310@gmail.com>
2025-07-16 13:45:10 -0300

    Repositories 'Criado a classe UsuarioRepository'

---
+++ src/main/java/com/portfolio/site/repositories/UsuarioRepository.java
@@ -0,0 +1,7 @@
+package com.portfolio.site.repositories;
+
+import com.portfolio.site.entities.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
