9967f12d716ae0cb34e652e45f56549a6e2bd060
Rodrigo <rodrigo.paz.2310@gmail.com>
2025-07-16 14:10:49 -0300

    Atualizado a interface ProdutoRepository 'Aplicando annotation Repository'

--- src/main/java/com/portfolio/site/repositories/ProdutoRepository.java
+++ src/main/java/com/portfolio/site/repositories/ProdutoRepository.java
@@ -2,6 +2,8 @@ package com.portfolio.site.repositories;
 
 import com.portfolio.site.entities.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 }
